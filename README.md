721c9c734756bf6c84a9821ec94f55c21345a3e2
itcrazy0717 <itcrazy0717@gmail.com>
2020-05-23 17:16:39 +0800

    feat(master)：增加相关注释

--- README.md
+++ README.md
@@ -1,13 +1,2 @@
 ### 根据mybatis-plus修改后的代码生成器
-1.spring-boot继承mybatis-plus依赖包：
-
-`<dependency>
-     <groupId>com.baomidou</groupId>
-     <artifactId>mybatis-plus-boot-starter</artifactId>
-     <version>3.3.1</version>
- </dependency>
- <dependency>
-     <groupId>org.mybatis.spring.boot</groupId>
-     <artifactId>mybatis-spring-boot-starter</artifactId>
-     <version>2.1.2</version>
- </dependency>`
+具体实现待续......
---
+++ src/main/java/com/baomidou/mybatisplus/generator/package-info.java
@@ -0,0 +1,19 @@
+/**
+ * @author: dengxin.chen
+ * @date: 2020-05-23 17:14
+ * @description:
+ */
+package com.baomidou.mybatisplus.generator;
+/**
+ * spring-boot继承mybatis-plus依赖包：
+ * <dependency>
+ *      <groupId>com.baomidou</groupId>
+ *      <artifactId>mybatis-plus-boot-starter</artifactId>
+ *      <version>3.3.1</version>
+ *  </dependency>
+ *  <dependency>
+ *      <groupId>org.mybatis.spring.boot</groupId>
+ *      <artifactId>mybatis-spring-boot-starter</artifactId>
+ *      <version>2.1.2</version>
+ *  </dependency>
+ */
